aedd7dc3a86264a16014aa059ee527b1f0d3cf62
roman <romanbrito1@gmail.com>
2018-06-03 12:22:46 -0500

    initial GraphQL server with Graphiql

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import expess from 'express'
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
 
-const app = expess()
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('GraphQL is great')
 })
+
+const root = { hello: () => "Hi, I'm Roman"} // resolver
+
+app.use('/graphql', graphqlHTTP({
+  schema, //same as schema:schema
+  rootValue: root,
+  graphiql: true, // to test
+}))
 
 app.listen(8080, () => console.log('Running on port localhost:8080/graphql'))
